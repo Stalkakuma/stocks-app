a830d802d768a417090ab873c2e77affe5712a0a
Stalkakuma <stalkakuma@gmail.com>
2022-08-16 14:06:54 +0300

    initial commit

---
+++ st-ui/src/App.tsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+const App = () => {
+  const [stockData, setStockData] = useState(null);
+
+  useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setStockData(data.message));
+  }, []);
+
+  return (
+    <div className="App">
+      <p>{!stockData ? "Loading..." : stockData}</p>
+    </div>
+  );
+};
+
+export default App;
